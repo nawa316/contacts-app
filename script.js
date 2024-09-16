10570647da83707e73945369ff884b5ab2c84b52
nawa316 <muhammadadedzakwan@gmail.com>
2024-09-16 07:09:15 +0700

    feat: adding erase feature

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 const contact = document.getElementById("contact");
+let contactData = [];
 
 function fetchData() {
   fetch("https://dummyjson.com/users")
     .then((response) => response.json())
-    .then((json) => renderData(json.users));
+    .then((json) => contactData.push(...json.users))
+    .then(() => renderData(contactData));
   return;
 }
 fetchData();
 
+function deleteContact(i) {
+  contactData[i] = null;
+  renderData(contactData);
+}
+
+function updateContact(i) {
+  document
+    .getElementById("button" + i)
+    .addEventListener("click", deleteContact(i));
+}
+
 function renderData(data) {
   let list = data
-    .map(
-      (user) =>
-        `<div class="container_contact_list">
+    .map((user, i) => {
+      if (user !== null) {
+        return `<div class="container_contact_list">
             <img src="${user.image}" alt="${user.firstName}" class="user-image"/>
             <div class="contact-detail">
                 <p>${user.firstName} ${user.lastName}</p>
                 <p>${user.email}</p>
             </div>
-            <button class="button2">x</button>
-        </div>`
-    )
+            <button id="button${i}" onclick="updateContact(${i})" class="button2">x</button>
+        </div>`;
+      } else {
+        return "";
+      }
+    })
     .join(" ");
   contact.innerHTML = list;
 }
